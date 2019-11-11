10aeb4e124995baf4f9b4a7c141451d6618f7933
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-11 15:11:11 -0800

    Dashboard tests complete

--- src/dashboard/Dashboard.test.js
+++ src/dashboard/Dashboard.test.js
@@ -1,17 +1,8 @@
 // Test away
 import React from "react"
-import {render, fireEvent} from "@testing-library/react";
+import {render} from "@testing-library/react";
 import Dashboard from "./Dashboard";
 
 test('Dashboard renders successfully', () => {
-    render(<Dashboard />);
+   expect(render(<Dashboard />)).toMatchSnapshot();
 })
-
-// test('Display components render successfully on load', () => {
-//     const {getByText} = render(<Dashboard />);
-//
-//     getByText(/unlocked/i);
-//     getByText(/open/i);
-//     getByText(/lock gate/i);
-//     getByText(/close gate/i);
-// });
