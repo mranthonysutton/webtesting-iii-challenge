2946097d989036b237c4556db6ce6d464d65c1c1
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-11 14:36:16 -0800

    Tests if lock gate and close gate renders in controls

--- src/controls/Controls.test.js
+++ src/controls/Controls.test.js
@@ -1,8 +1,15 @@
 // Test away
 import React from "react"
-import {render, fireEvent} from "@testing-library/react";
+import {render} from "@testing-library/react";
 import Controls from "./Controls";
 
 test('Controls renders successfully', () => {
-    render(<Controls />)
+    render(<Controls/>)
+})
+
+test('Close Gate and Lock Gate on screen', () => {
+    const controls = render(<Controls/>)
+
+    controls.getByText(/close gate/i);
+    controls.getByText(/lock gate/i);
 })
