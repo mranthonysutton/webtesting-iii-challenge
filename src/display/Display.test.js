664e14adaf560a9405edec06fcb1791b281d798f
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-11 12:52:00 -0800

    Display, controls, and dashboard render successfully. Need to build out tests for the functions

--- src/display/Display.test.js
+++ src/display/Display.test.js
@@ -1,1 +1,8 @@
-// Test away!
+// Test away
+import React from "react"
+import {render, fireEvent} from "@testing-library/react";
+import Display from "./Display";
+
+test('Display renders successfully', () => {
+    render(<Display />)
+})
